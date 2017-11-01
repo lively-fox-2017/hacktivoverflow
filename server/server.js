e01e5f10071655930f65c9af43481573e93d6f8d
Dimitri Wahyudiputra <deathmitri@gmail.com>
2017-11-01 19:00:43 +0700

    Added server.js

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const server = express();
+const port = process.env.PORT || 3000;
+
+const apiRoutes = require('./routes');
+
+server.use(cors());
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.use('/', apiRoutes);
+
+server.use(() => {
+  res.status(404).json({
+    error: 'Route not found'
+  });
+});
+
+server.listen(port, () => {
+  console.log('Server running on port', port);
+});
