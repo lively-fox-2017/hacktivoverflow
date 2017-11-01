d37ffe7235d8ffaefcfbcf83e96578ae13d325d9
Dimitri Wahyudiputra <deathmitri@gmail.com>
2017-11-01 19:34:34 +0700

    Added basic login and register

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const authRoutes = require('./auth');
+
+router.use('/auth', authRoutes);
+
 router.get('/', (req, res) => {
   res.status(200).json({
     message: 'HacktivOverflow API Index'
