d1b818248005495aa39a2c5ec11a13ab48deae19
redhaputra <putra.redha@gmail.com>
2017-11-05 10:40:10 +0700

    add and setup client

---
+++ Client/src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './vuex/store'
+import axios from 'axios'
+import swal from 'vue-sweetalert2'
+import VueJWT from 'vuejs-jwt'
+
+Vue.prototype.$axios = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+Vue.use(swal)
+Vue.use(VueJWT, {
+  signKey: 'hacktiv8',
+  keyName: 'token'
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
---
+++ Client/src/vuex/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// import axios from 'axios'
+
+Vue.use(Vuex)
+
+// let db = axios.create({
+//   baseURL: 'http://localhost:3000'
+// })
+
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  }
+})
+
+export default store
