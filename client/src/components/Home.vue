a0670e025f0f49c8296d8a7eda539a6da50f1134
Dimitri Wahyudiputra <deathmitri@gmail.com>
2017-11-05 11:39:26 +0700

    Finished basic home template with navbar

---
+++ client/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Welcome to HacktivOverflow</h1>
+    <p>To post a question/answer you have to <a href="#">login</a> first</p>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style></style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">HacktivOverflow</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li>
+            <router-link to="questions">Questions</router-link>
+          </li>
+        </ul>
+        <div class="navbar-form navbar-right">
+          <router-link to="login" class="btn btn-primary">Login</router-link>
+          <router-link to="logout" class="btn btn-danger">Logout</router-link>
+        </div>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+  .navbar-inverse {
+    border-radius: 0px;
+  }
+</style>
