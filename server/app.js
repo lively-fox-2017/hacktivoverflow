2d2b1f7b2f1cac964ae921043f37ad94a7bb04c3
lokiloko <ianw.tkj2@gmail.com>
2017-11-06 13:10:42 +0700

    add https and htpp server

--- server/app.js
+++ server/app.js
@@ -1,4 +1,11 @@
 require('dotenv').config()
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -20,4 +27,8 @@ app.use('/api/auth', auth)
 app.use('/api/questions', question)
 app.use('/api/answers', answer)
 
-app.listen(3000)
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(3000);
+httpsServer.listen(443);
